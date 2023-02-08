e02b7308d9391232b7bd1a5acb8f6ee94310459b
xiaoriri <admin@xiaoriri.com>
2023-02-08 17:25:28 +0800

    index

--- index.php
+++ index.php
@@ -5,16 +5,10 @@
  */
 
 use roc\Application;
-use roc\Context;
-use roc\Middleware\CorsMiddleware;
-use roc\Middleware\MetricMiddleware;
-use roc\RocServer;
-use roc\Router\Router;
-
+use roc\Container;
 
 ini_set('display_errors', 'on');
 ini_set('display_startup_errors', 'on');
-
 error_reporting(E_ALL);
 date_default_timezone_set('Asia/Shanghai');
 
@@ -27,27 +21,10 @@ if (file_exists($dir . '/vendor/autoload.php')) {
 
 
 require_once __DIR__ . '/vendor/autoload.php';
-$app = \roc\Container::getInstance(Application::class);
+
+/**
+ * @var Application $app
+ */
+$app = Container::getInstance(Application::class);
 $app->run();
-//Router::get('/hello', function (Context $context) {
-//    $context->writeJson(['code' => 200, 'msg' => '小日日12312']);
-//});
-//Router::get('/test', [\roc\Test::class, 'test']);
-//$server = new RocServer('0.0.0.0', 9501, [
-//    new CorsMiddleware(),
-//    new MetricMiddleware(),
-//]);
-////$server->setRoute('get', '/hello', function (Context $context) {
-////    $context->writeJson(['code' => 200, 'msg' => '小日日']);
-////});
-////$server->setRoute('get', '/hello', [Test::class, 'test']);
-//$server->start();
-
-//$definitionSource = new DefinitionSource([IRoutes::class => Routes::class]);
-//$container = new Container($definitionSource);
-//$routers = $container->get(IRoutes::class);
-//$routers->get("aaa", function () {
-//    return "aaa";
-//});
-
 
